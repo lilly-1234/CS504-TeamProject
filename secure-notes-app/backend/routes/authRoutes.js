f09650d7ea5063821476ff95d26e3ee3d7d065f8
Rajdeep Singh Golan <rajdeepsinghgolan@Rajdeeps-MacBook-Air.local>
2025-05-21 22:13:24 -0700

    2FA added

--- secure-notes-app/backend/routes/authRoutes.js
+++ secure-notes-app/backend/routes/authRoutes.js
@@ -7,6 +7,7 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login); 
 
 router.post('/mfa/generate', authController.generateMFASecret);
+
 router.post('/mfa/verify', authController.verifyMFA);
 
 module.exports = router;
